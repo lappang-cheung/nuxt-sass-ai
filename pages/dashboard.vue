c8275073e001fdb6e859600b2e77c595a749d546
Leo Cheung <lappangcheung@gmail.com>
2024-10-03 01:56:28 -0400

    Setting up the dashboard

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -4,7 +4,27 @@
 
 <template>
   <div>
-    Dashboard
+    <div class="mb-8">
+      <h2>Explore the power of AI</h2>
+      <p>Chat with the smartest AI - Explore the power of AI</p>
+    </div>
+    <div>
+      <div v-for="tool in tools"
+           :key="tool.label"
+           @click="navigateTo(`/${tool.link}`)"
+           class="p-4 border-muted border flex items-center justify-between hover:shadow-sm transition cursor-pointer">
+        <div class="flex items-center gap-x-4">
+          <div :class="`p-2 w-fit rounded-md ${tool.bgColor}`">
+            <Icon :name="tool.icon" :class="`h-8 w-8 ${tool.color}`" />
+          </div>
+          <div class="font-semibold">
+            {{ tool.label }}
+          </div>
+        </div>
+        <Icon name="lucide:arrow-right"
+              class="w-5 h-5"/>
+      </div>
+    </div>
   </div>
 </template>
 
--- utils/index.ts
+++ utils/index.ts
@@ -41,3 +41,54 @@ export const dashboardLinks = [
     color: 'text-green-700'
   }
 ]
+
+export const tools = [
+  {
+    label: 'Dashboard',
+    icon: 'lucide:layout-dashboard',
+    link: '/dashboard',
+    color: 'text-sky-500',
+    bgColor: 'text-sky-500/10',
+  }, {
+    label: 'Conversation',
+    icon: 'lucide:message-square',
+    link: '/conversation',
+    color: 'text-violet-500',
+    bgColor: 'text-violet-500/10'
+  },
+  {
+    label: 'Image',
+    icon: 'lucide:image',
+    link: '/image',
+    color: 'text-pink-500',
+    bgColor: 'text-pink-500/10'
+  },
+  {
+    label: 'Video',
+    icon: 'lucide:video',
+    link: '/video',
+    color: 'text-orange-700',
+    bgColor: 'text-orange-700/10'
+  },
+  {
+    label: 'Music',
+    icon: 'lucide:music',
+    link: '/music',
+    color: 'text-emerald-700',
+    bgColor: 'text-emerald-700/10'
+  },
+  {
+    label: 'Code',
+    icon: 'lucide:code',
+    link: '/code',
+    color: 'text-green-700',
+    bgColor: 'text-green-700/10'
+  },
+  {
+    label: 'Settings',
+    icon: 'lucide:settings',
+    link: '/settings',
+    color: 'text-green-700',
+    bgColor: 'text-green-700/10'
+  }
+]
